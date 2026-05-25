85daf60dcc30e0c41a2e3d44c4ba5c4fd0e021bf
Haeyeon <121421927+m2za5@users.noreply.github.com>
2026-05-26 01:34:25 +0900

    Create AltarTimelineComponent.h

---
+++ Source/GamePlay/Public/AltarTimelineComponent.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SceneComponent.h"
+#include "Components/TimelineComponent.h"
+#include "Engine/PostProcessVolume.h"
+#include "AltarTimelineComponent.generated.h"
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class LUDENS7_TEAMPROJECT_API UAltarTimelineComponent : public UActorComponent
+{
+    GENERATED_BODY()
+
+public:
+    UAltarTimelineComponent();
+ 
+private:
+   
+};
