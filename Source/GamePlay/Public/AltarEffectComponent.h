7fecfb6b56d77049b2f83eab4981a2187d6b6cef
Haeyeon <121421927+m2za5@users.noreply.github.com>
2026-05-26 01:33:58 +0900

    Create AltarEffectComponent.h

---
+++ Source/GamePlay/Public/AltarEffectComponent.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SceneComponent.h"
+#include "Particles/ParticleSystem.h"
+#include "Components/StaticMeshComponent.h"
+#include "AltarEffectComponent.generated.h"
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class LUDENS7_TEAMPROJECT_API UAltarEffectComponent : public UActorComponent
+{
+    GENERATED_BODY()
+
+public:
+    UAltarEffectComponent();
+
+   
+
+private:
+    
+};
