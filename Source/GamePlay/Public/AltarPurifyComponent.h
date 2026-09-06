d9533811c7f32dcd0b5210490ea67b8dd7516efb
Haeyeon <121421927+m2za5@users.noreply.github.com>
2026-09-07 04:33:09 +0900

    Update AltarPurifyComponent.h

--- Source/GamePlay/Public/AltarPurifyComponent.h
+++ Source/GamePlay/Public/AltarPurifyComponent.h
@@ -10,30 +10,38 @@
 class UCutsceneManager;
 class ULevelSequence;
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnPurifyCutscenesFinished);
+
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class LUDENS7_TEAMPROJECT_API UAltarPurifyComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
-public:	
+public:
 	UAltarPurifyComponent();
 
+	UFUNCTION(BlueprintCallable, Category = "Cutscene")
 	void PlayPurifyCutscene();
 
 	UFUNCTION()
 	void OnCutsceneEnded(FName FinishedCutsceneID);
 
+	UPROPERTY(BlueprintAssignable, Category = "Cutscene")
+	FOnPurifyCutscenesFinished OnPurifyCutscenesFinished;
 
 protected:
 	virtual void BeginPlay() override;
-
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 
 private:
 	FName GetCurrentCutsceneID() const;
+	UCutsceneManager* GetCutsceneManager() const;
 
 	UPROPERTY(EditAnywhere, Category = "Cutscene")
-	TArray<ULevelSequence*> CutsceneSequenceList;
+	TArray<TObjectPtr<ULevelSequence>> CutsceneSequenceList;
 
 	bool bCutsceneStarted = false;
 	int32 CurrentCutsceneIndex = 0;
+
+	FName ActivePurifyCutsceneID = NAME_None;
 };
